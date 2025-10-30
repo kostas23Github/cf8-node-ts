3289cf2c86d71a6a4ed142d05de783bc18c1d7b6
kostas23Github <kanastasiou.dev@gmail.com>
2025-10-30 21:40:32 +0200

    Feat: Add main entry point of api.

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import morgan from "morgan";
+import userRoutes from "./routes/user.routes";
+import roleRoutes from "./routes/role.routes";
+import authRoutes from "./routes/auth.routes";
+
+const app = express();
+
+app.use(morgan("dev")); // HTTP request logger middleware for terminal
+app.use(express.json()); // Parse incoming JSON requests
+
+app.use("/api/users", userRoutes);
+app.use("/api/roles", roleRoutes);
+app.use("api/auth", authRoutes);
+
+export default app;
