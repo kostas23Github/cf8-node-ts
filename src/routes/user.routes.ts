bdc4242b20f3ace3c25a027905fdcc99dcc4e8fc
kostas23Github <kanastasiou.dev@gmail.com>
2025-10-31 22:58:05 +0200

    Feat: Implement authenticate function to role & user routes, to restrict access.

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,16 +3,17 @@ import * as userCtrl from "../controllers/user.controller";
 import { validate } from "../middlewares/validate.middleware";
 import { validateObjectId } from "../middlewares/validateObjectId.middleware";
 import { createUserSchema, updateUserSchema } from "../validators/user.validator";
+import { authenticate } from '../middlewares/auth.middleware';
 
 const router = Router();
 
-router.get("/", userCtrl.list);
+router.get("/", authenticate, userCtrl.list);
 // Add validation middleware only for specific routes.
 // Why add it here?
 // To aliviate the API from validation logic further down the line(i.e. the db itself), since it can happen right away.
 router.get("/:id", validateObjectId('id'), userCtrl.getOne);  // 'id' is redundant here but explicit for clarity.
-router.post("/", validate(createUserSchema), userCtrl.create);
-router.put("/:id", validate(updateUserSchema), validateObjectId('id'), userCtrl.update);
-router.delete("/:id", validateObjectId('id'), userCtrl.remove);
+router.post("/", authenticate, validate(createUserSchema), userCtrl.create);
+router.put("/:id", authenticate, validate(updateUserSchema), validateObjectId('id'), userCtrl.update);
+router.delete("/:id", authenticate, validateObjectId('id'), userCtrl.remove);
 
 export default router;
