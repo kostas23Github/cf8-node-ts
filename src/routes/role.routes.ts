d09e306838eaaa706cce9bdaae3463ef3ea85a68
kostas23Github <kanastasiou.dev@gmail.com>
2025-10-31 22:23:36 +0200

    Feat: Implement new validations to role routes as middlewares.

--- src/routes/role.routes.ts
+++ src/routes/role.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import * as roleCtrl from "../controllers/role.controller";
+import { validate } from "../middlewares/validate.middleware";
+import { validateObjectId } from "../middlewares/validateObjectId.middleware";
+import { createRoleSchema, updateRoleSchema } from "../validators/role.validator";
 
 const router = Router();
 
 router.get("/", roleCtrl.list);
-router.post("/", roleCtrl.create);
-router.put("/:id", roleCtrl.update);
-router.delete("/:id", roleCtrl.remove);
+router.post("/", validate(createRoleSchema), roleCtrl.create);
+router.put("/:id", validateObjectId('id'), validate(updateRoleSchema), roleCtrl.update);
+router.delete("/:id", validateObjectId('id'), roleCtrl.remove);
 
 export default router;
