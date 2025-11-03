b6c265c268580a9a40a3cc317b52eafaa4e01fb2
kostas23Github <kanastasiou.dev@gmail.com>
2025-11-03 20:15:59 +0200

    Feat: Add swagger to entry point of app.

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,14 @@ import morgan from "morgan";
 import userRoutes from "./routes/user.routes";
 import roleRoutes from "./routes/role.routes";
 import authRoutes from "./routes/auth.routes";
+import { setupSwagger } from "./swagger";
 
 const app = express();
 
-app.use(morgan("dev")); // HTTP request logger middleware for terminal
-app.use(express.json()); // Parse incoming JSON requests
+setupSwagger(app);
+
+app.use(morgan("dev"));  // HTTP request logger middleware for terminal.
+app.use(express.json()); // Parse incoming JSON requests.
 
 app.use("/api/users", userRoutes);
 app.use("/api/roles", roleRoutes);
