0fbb2f7d8ece9f4e59db2b1e7d22f269b5265f48
kostas23Github <kanastasiou.dev@gmail.com>
2025-11-03 20:14:13 +0200

    Feat: Add user get all & post swagger docs.

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -7,11 +7,54 @@ import { authenticate } from '../middlewares/auth.middleware';
 
 const router = Router();
 
+/**
+ * @openapi
+ * /users:
+ *  get:
+ *    summary: List of all users
+ *    tags: [Users]
+ *    security:
+ *      - bearerAuth: []
+ *    responses:
+ *      200:
+ *        description: Response list of users
+ */
 router.get("/", authenticate, userCtrl.list); // For someone to be able to get the list of users, he/she must be authenticated, meaning having a valid token.
 // Add validation middleware only for specific routes.
 // Why add it here?
 // To aliviate the API from validation logic further down the line(i.e. the db itself), since it can happen right away.
 router.get("/:id", validateObjectId('id'), userCtrl.getOne);  // 'id' is redundant here but explicit for clarity.
+
+/**
+ * @openapi
+ * /users:
+ *  post:
+ *    summary: Creates a user
+ *    tags: [Users]
+ *    security:
+ *      - bearerAuth: []
+ *    requestBody:
+ *      required: true
+ *      content: 
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              username:
+ *                type: string
+ *              password:
+ *                type: string
+ *              firstname:
+ *                type: string
+ *              lastname:
+ *                type: string
+ *              email:
+ *                type: string
+ *    responses: 
+ *      201:
+ *        description: User created
+ * 
+ */
 router.post("/", authenticate, validate(createUserSchema), userCtrl.create);
 router.put("/:id", authenticate, validate(updateUserSchema), validateObjectId('id'), userCtrl.update);
 router.delete("/:id", authenticate, validateObjectId('id'), userCtrl.remove);
