84e4e3511191dbf4aae7a6b2c7fc0ae912e74f12
kostas23Github <kanastasiou.dev@gmail.com>
2025-11-04 20:41:14 +0200

    Feat: Implement 3 test cases.

---
+++ src/tests/user.integration.test.ts
@@ -0,0 +1,64 @@
+import { TestServer } from "./testSetup";
+import userRoutes from "../routes/user.routes";
+import User from "../models/user.model";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { describe } from "node:test";
+import dotenv from "dotenv";
+
+dotenv.config();
+const JWT_SECRET = process.env.JWT_SECRET || "";
+const server = new TestServer();
+server.app.use("/users", userRoutes);
+
+describe("User API Tests", () => {
+
+  let token: string;
+
+  beforeAll(async () => {
+    await server.start();
+    const hash = await bcrypt.hash("admin1234", 10);
+    const user = await User.create(
+      {
+        username: "admin",
+        password: hash,
+        firstname: "testUser",
+        lastname: "testUser",
+        email: "testUser@aueb.gr"
+      }
+    );
+    const payload = {
+      username: user.username,
+      email: user.email,
+      roles: user.roles
+    };
+    token = jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
+  });
+
+
+  test(
+    "GET /users -> returns list of users",
+    async () => {
+      const res = await server.request.get("/users").set("Authorization", `Bearer ${token}`);
+      expect(res.status).toBe(200);
+      expect(Array.isArray(res.body)).toBe(true);
+    });
+
+  test("POST /users -> creates new user", async () => {
+    const res = await server.request.post("/users")
+      .set("Authorization", `Bearer ${token}`)
+      .send({ username: "newuser", password: "123456" });
+    expect(res.status).toBe(201);
+    expect(res.body.username).toBe("newuser");
+  });
+
+  test("POST /users -> creates new user", async () => {
+    const res = await server.request.post("/users")
+      .set("Authorization", `Bearer ${token}`)
+      .send({ username: "newuser", password: "12" });
+    expect(res.status).toBe(400);
+  });
+
+  afterAll(async () => { await server.stop(); });
+
+})
