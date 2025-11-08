ade9b0f4bccae2affa79d3448dcbf02191e9b7bc
kostas23Github <kanastasiou.dev@gmail.com>
2025-11-08 23:34:11 +0200

    Feat: Add cors packages & cors allowed urls.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
+
 import userRoutes from "./routes/user.routes";
 import roleRoutes from "./routes/role.routes";
 import authRoutes from "./routes/auth.routes";
@@ -11,6 +13,9 @@ setupSwagger(app);
 
 app.use(morgan("dev"));  // HTTP request logger middleware for terminal.
 app.use(express.json()); // Parse incoming JSON requests.
+app.use(cors({
+  origin: ["http://localhost:4200", "https://www.aueb.gr"]
+})); // Enable CORS for these routes.
 
 app.use("/api/users", userRoutes);
 app.use("/api/roles", roleRoutes);
